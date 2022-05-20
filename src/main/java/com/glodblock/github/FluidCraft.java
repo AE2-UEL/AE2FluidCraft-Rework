9096734722fa1972a172ad599866f84b674889ac
GlodBlock <1356392126@qq.com>
2022-05-20 21:25:46 +0800

    bump version

--- src/main/java/com/glodblock/github/FluidCraft.java
+++ src/main/java/com/glodblock/github/FluidCraft.java
@@ -17,7 +17,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class FluidCraft {
 
     public static final String MODID = "ae2fc";
-    public static final String VERSION = "2.0.0-r";
+    public static final String VERSION = "2.1.0-r";
 
     @Mod.Instance(MODID)
     public static FluidCraft INSTANCE;
