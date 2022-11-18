a3bbba1486749acc604ad13632ecdfec3e31993d
GlodBlock <1356392126@qq.com>
2022-11-18 17:56:57 +0800

    v2.4.7

--- src/main/java/com/glodblock/github/FluidCraft.java
+++ src/main/java/com/glodblock/github/FluidCraft.java
@@ -20,7 +20,7 @@ import org.apache.logging.log4j.Logger;
 public class FluidCraft {
 
     public static final String MODID = "ae2fc";
-    public static final String VERSION = "2.4.6-r";
+    public static final String VERSION = "2.4.7-r";
 
     @Mod.Instance(MODID)
     public static FluidCraft INSTANCE;
