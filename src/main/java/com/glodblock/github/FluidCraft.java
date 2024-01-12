a798cea9f4e5a43bb40c998409a05a4136e60abb
GlodBlock <1356392126@qq.com>
2024-01-12 19:13:55 +0800

    bump version

--- src/main/java/com/glodblock/github/FluidCraft.java
+++ src/main/java/com/glodblock/github/FluidCraft.java
@@ -14,7 +14,7 @@ import org.apache.logging.log4j.Logger;
 public class FluidCraft {
 
     public static final String MODID = "ae2fc";
-    public static final String VERSION = "2.5.3-r";
+    public static final String VERSION = "2.5.4-r";
     public static final boolean beta = false;
 
     @Mod.Instance(MODID)
