ed47f7da6737f77af0c6365476ab4ff45dab8615
GlodBlock <1356392126@qq.com>
2024-01-12 19:12:48 +0800

    fix crash when gtce is loaded

--- src/main/java/com/glodblock/github/integration/gregtech/GregUtil.java
+++ src/main/java/com/glodblock/github/integration/gregtech/GregUtil.java
@@ -1,14 +1,32 @@
 package com.glodblock.github.integration.gregtech;
 
+import com.glodblock.github.util.Ae2Reflect;
 import gregtech.integration.jei.recipe.GTRecipeWrapper;
 import mezz.jei.api.recipe.IRecipeWrapper;
 
+import java.lang.reflect.Method;
+
 public class GregUtil {
 
+    private static Method mGTRecipeWrapper_isNotConsumedItem;
+
+    static {
+        try {
+            mGTRecipeWrapper_isNotConsumedItem = Ae2Reflect.reflectMethod(GTRecipeWrapper.class, "isNotConsumedItem", int.class);
+        } catch (NoSuchMethodException e) {
+            mGTRecipeWrapper_isNotConsumedItem = null;
+        }
+    }
+
+    // because gtce and gtceu share the same id
     public static boolean isNotConsume(IRecipeWrapper wrapper, int index) {
-        if (wrapper instanceof GTRecipeWrapper) {
-            GTRecipeWrapper gtRecipe = (GTRecipeWrapper) wrapper;
-            return gtRecipe.isNotConsumedItem(index);
+        if (wrapper instanceof GTRecipeWrapper && mGTRecipeWrapper_isNotConsumedItem != null) {
+            try {
+                GTRecipeWrapper gtRecipe = (GTRecipeWrapper) wrapper;
+                return (boolean) mGTRecipeWrapper_isNotConsumedItem.invoke(gtRecipe, index);
+            } catch (Exception e) {
+                return false;
+            }
         }
         return false;
     }
